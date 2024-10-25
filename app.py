49fe3350a830d520152befe7aa4d7c8bdb08d985
Mohamed Ehab <104400937+ehab1441@users.noreply.github.com>
2024-10-26 02:43:18 +0300

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ genre = st.selectbox("Genre", options=['Action', 'Crime', 'Thriller', 'Animation
        'Fantasy', 'Romance', 'Drama', 'Western', 'Family', 'Comedy',
        'Adventure', 'Mystery', 'TV', 'Documentary', 'War', 'Music',
        'History'])  # add your options here
-popularity = st.selectbox("Popularity", options=[1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020])
+popularity = st.selectbox("Popularity",  options=['Very High', 'High', 'Medium', 'Low'])
 language = st.selectbox("Language", options=['English', 'Japanese', 'French', 'Hindi', 'Spanish', 'Russian',
        'German', 'Thai', 'Korean', 'Turkish', 'Chinese', 'Italian',
        'Portuguese', 'Malayalam', 'Polish', 'Finnish', 'Norwegian',
@@ -13,7 +13,7 @@ language = st.selectbox("Language", options=['English', 'Japanese', 'French', 'H
        'Norwegian Bokmål', 'Basque', 'Latvian', 'Arabic', 'Greek',
        'Czech', 'Malay', 'Bengali', 'Catalan', 'Latin', 'Tamil',
        'Hungarian', 'Hebrew', 'Estonian'])
-st.selectbox("Release Era", options=['Very High', 'High', 'Medium', 'Low'])
+st.selectbox("Release Era", options=[1900, 1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020])
 
 if st.button("Get Recommendations"):
     recommendations = recommend_movies(genre, popularity, language, release_era)
